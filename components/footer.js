207ae758bfc0d3b3cf60574dbd1b875491d0be02
Anshuman Mohanty <58540815+Anshuman2305@users.noreply.github.com>
2022-09-11 22:58:58 +0530

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="flex p-16">
+    <div className="flex p-[3em]">
       <div className="footer flex flex-1 justify-around">
         <p>इस वेबसाइट को बनाने में सिर्फ <span id="footer" className="font-bold text-[#3a3a3a] dark:text-[#d3d3d3]">ब्लैक पेन</span> का इस्तेमाल किया गया है</p>
       </div>
